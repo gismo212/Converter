e56f4455b47b20a59f9badd95c5bcf0ebef1085a
Elegard <whiteandy21321@gmail.com>
2023-04-11 21:35:37 +0300

    Update /form.erb,create conclusion user information

--- app.rb
+++ app.rb
@@ -8,12 +8,21 @@ require 'sinatra/reloader'
 
 
 get '/' do
-	erb 'In order to use the currency converter, go to the converter tab!'
+	erb 'In order to use the currency converter, go to the Form tab!'
 end
 
 
-get'/Converter' do
-   erb:Converter
+get '/Form' do
+   erb :Form
 end
 
+post '/Form' do
+	@login=params[:login]
+	@email=params[:mail]
+	erb "You login:#{@login}, You email:#{@email}"
+end
+
+get '/Converter' do
+	erb :Converter
+end
 
