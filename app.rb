a0a8666a6eae7eaad0d0cb7ad79ad5f969094240
Elegard <whiteandy21321@gmail.com>
2023-05-16 12:25:54 +0300

    output selected values from converter form

--- app.rb
+++ app.rb
@@ -7,27 +7,28 @@ require 'sqlite3'
 #3 оформить переход на на конвертер при авторизации 
 
 
-def get_db
-	db=SQLite3::Database.new 'Users.db'
-	db.results_as_hash=true
-	return db
-end
+# def get_db
+# 	db=SQLite3::Database.new 'Users.db'
+# 	db.results_as_hash=true
+# 	return db
+# end
 
-configure do
-	db=get_db
+# configure do
+# 	db=get_db
 
-	db.execute'CREATE TABLE IF NOT EXISTS "Users"
-	 (
-	 "Id" INTEGER PRIMARY KEY AUTOINCREMENT,
-	  "Email" VARCHAR,
-	   "Login" VARCHAR
-	   )'
-end
+# 	db.execute'CREATE TABLE IF NOT EXISTS "Users"
+# 	 (
+# 	 "Id" INTEGER PRIMARY KEY AUTOINCREMENT,
+# 	  "Email" VARCHAR,
+# 	   "Login" VARCHAR
+# 	   )'
+# end
 
 
 
 get '/' do
-	erb 'In order to use the currency converter, go to the Form tab!'
+	# 'In order to use the currency converter, go to the Form tab!'
+	redirect '/Converter'
 end
 
 
@@ -35,42 +36,45 @@ get '/Form' do
    erb :Form
 end
 
-post '/Form' do
-	@login=params[:login]
-	@email=params[:mail]
-	#erb "You login:#{@login}, You email:#{@email}"
-	hh={:login=>'Enter you login',:mail=>'Enter you email'}
+# post '/Form' do
+# 	@login=params[:login]
+# 	@email=params[:mail]
+# 	#erb "You login:#{@login}, You email:#{@email}"
+# 	hh={:login=>'Enter you login',:mail=>'Enter you email'}
 
-	hh.each do |key,value| 
-			if params[key]==''
-				@error=hh[key]
-				return erb :Form
-			end	
-	end
+# 	hh.each do |key,value| 
+# 			if params[key]==''
+# 				@error=hh[key]
+# 				return erb :Form
+# 			end	
+# 	end
 
-	if @login=='admin' && @email=='bric98@mail.ru'
-		erb:info	
-	end	
-	erb :Converter
-end
+# 	if @login=='admin' && @email=='bric98@mail.ru'
+# 		erb:info	
+# 	end	
+# 	erb :Converter
+# end
 	
-	get '/info' do
-		db=get_db
-		db.execute'select *from Users  id order by id',[id]
-		erb:info
-	end
+# 	get '/info' do
+# 		db=get_db
+# 		db.execute'select *from Users  id order by id',[id]
+# 		erb:info
+# 	end
 
-	post '/info'  do
-		db=get_db
-		db.execute'insert into Users ("Email","Login") values(?,?)',[@email,@login]
-		erb:info
-	end
+# 	post '/info'  do
+# 		db=get_db
+# 		db.execute'insert into Users ("Email","Login") values(?,?)',[@email,@login]
+# 		erb:info
+# 	end
 
 
 get '/Converter' do
 	erb :Converter
 end
 
-post 'Converter' do
-	erb :Converter
+post '/Converter' do
+	well=params[:well]
+	d=params[:usd]
+	b=params[:brb]
+	erb "well:#{well},usd:#{d},brb:#{b} "
 end
